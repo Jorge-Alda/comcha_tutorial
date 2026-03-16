67b9393478a65fed7a0d760c81b08da02238ea0b
Jorge-Alda <jorge.alda@pd.infn.it>
2026-03-16 19:59:46 +0100

    classifier

---
+++ alps/alpaca_chi2.py
@@ -0,0 +1,7 @@
+import alpaca
+import numpy as np
+
+def chi2(fa, cqL33, cuR, cdR, clL33, ceR):
+    c = alpaca.ALPcouplings({'cqL': np.diag([0,0,cqL33]), 'cuR': cuR, 'cdR': cdR, 'clL': np.diag([0,0,clL33]), 'ceR': ceR}, basis='derivative_above', scale=4*np.pi*fa).match_run(2.0, 'VA_below')
+    chi2 = alpaca.statistics.get_chi2(alpaca.sectors.default_sectors['all'], 2.0, c, fa, integrator='no_rge')
+    return chi2.chi2_tot()[0]
